0ca92d0be20284df010568059534ecb7aae183b7
evansosei0707 <evansosei0707@gmail.com>
2026-01-06 06:05:23 +0000

    Add import block for existing Lambda function

--- infrastructure/dev/import.tf
+++ infrastructure/dev/import.tf
@@ -1,11 +1,9 @@
 #------------------------------------------------------------------------------
-# Import Block for Existing CloudWatch Log Group
+# Import Blocks for Existing Resources
 #------------------------------------------------------------------------------
-# This import block will automatically import the existing log group into
+# These import blocks will automatically import existing resources into
 # Terraform state on the next terraform plan/apply, preventing the
-# "ResourceAlreadyExistsException" error.
-#
-# Import blocks must be in the root module, not in child modules.
+# "ResourceAlreadyExistsException" errors.
 #
 # After the first successful apply, this file can be safely deleted.
 #------------------------------------------------------------------------------
@@ -13,4 +11,9 @@
 import {
   to = module.lambda.aws_cloudwatch_log_group.lambda
   id = "/aws/lambda/enterprise-cicd-dev-release-metadata"
+}
+
+import {
+  to = module.lambda.aws_lambda_function.release_metadata
+  id = "enterprise-cicd-dev-release-metadata"
 }
